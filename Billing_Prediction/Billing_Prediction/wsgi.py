90cf8d04035a07a69405cedbdbb466b449cefc7a
sjdobaria <sjdobaria@gmail.com>
2026-01-25 18:48:00 +0530

    added

--- Billing_Prediction/Billing_Prediction/wsgi.py
+++ Billing_Prediction/Billing_Prediction/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Billing_Prediction.Billing_Prediction.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Billing_Prediction.settings')
 
 application = get_wsgi_application()
